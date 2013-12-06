3443729a6bc9f37949bfac0f4f2cf0324a6d00bb
Joshua Wood <josh@hintmedia.com>
2013-12-06 15:44:56 -0800

    Bump version to 1.10.beta2

--- lib/honeybadger.rb
+++ lib/honeybadger.rb
@@ -16,7 +16,7 @@ require 'honeybadger/user_feedback'
 require 'honeybadger/railtie' if defined?(Rails::Railtie)
 
 module Honeybadger
-  VERSION = '1.10.0.beta1'
+  VERSION = '1.10.0.beta2'
   LOG_PREFIX = "** [Honeybadger] "
 
   HEADERS = {
