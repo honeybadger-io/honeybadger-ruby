9bf6c323bc150ed02f005681be66cad722e316a5
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-04-10 11:55:30 -0700

    With a generic instrument method, Trace is now usable outside of Rails

--- lib/honeybadger/monitor.rb
+++ lib/honeybadger/monitor.rb
@@ -1,8 +1,8 @@
 require "honeybadger/array"
 require "honeybadger/monitor/sender"
 require "honeybadger/monitor/worker"
+require "honeybadger/monitor/trace"
 require "honeybadger/monitor/railtie" if defined?(Rails)
-require "honeybadger/monitor/trace" if defined?(Rails)
 
 module Honeybadger
   module Monitor
