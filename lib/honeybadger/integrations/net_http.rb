201e41d1d9aaad201c5be86d3bbace75bb9d445c
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-04-18 14:29:58 -0700

    Be less aggressive about instrumenting net::http

--- lib/honeybadger/integrations/net_http.rb
+++ lib/honeybadger/integrations/net_http.rb
@@ -6,13 +6,18 @@ module Honeybadger
 
       Net::HTTP.class_eval do
         def request_with_honeybadger(*args, &block)
-          request = args[0]
-          uri = request.path.match(%r{https?://}) ? URI(request.path) : URI("http#{use_ssl? ? 's' : ''}://#{address}:#{port}#{request.path}")
-          payload = { uri: uri, method: request.method }
-          ActiveSupport::Notifications.instrument("net_http.request", payload) do
+          if started? # Only instrument once we're underway
+            request = args[0]
+            uri = request.path.match(%r{https?://}) ? URI(request.path) : URI("http#{use_ssl? ? 's' : ''}://#{address}:#{port}#{request.path}")
+            payload = { uri: uri, method: request.method }
+            ActiveSupport::Notifications.instrument("net_http.request", payload) do
+              request_without_honeybadger(*args, &block)
+            end
+          else
             request_without_honeybadger(*args, &block)
           end
         end
+
         alias request_without_honeybadger request
         alias request request_with_honeybadger
       end
