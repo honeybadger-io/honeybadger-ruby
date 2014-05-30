57f0fd05fc6053cfa9e12f519a639d1101ade67f
Joshua Wood <josh@hintmedia.com>
2014-05-29 21:02:41 -0700

    Fix net_http_spec

--- spec/honeybadger/integrations/net_http_spec.rb
+++ spec/honeybadger/integrations/net_http_spec.rb
@@ -1,28 +1,29 @@
 require 'spec_helper'
 
-module ActiveSupport; end
+begin
+  require 'active_support/notifications'
+rescue LoadError
+  nil
+end
 
 describe "Net::HTTP Dependency" do
   before do
     Honeybadger::Dependency.reset!
   end
 
-  context "when net/http is not required" do
-    it "does not install instrumentation" do
-      Honeybadger::Dependency.inject!
-      expect(Net::HTTP.instance_methods).not_to include(:request_with_honeybadger)
-    end
-  end
-
-  context "when active_support notifications is required" do
-    before do
-      class ActiveSupport::Notifications
+  if defined?(ActiveSupport::Notifications)
+    context "when active support notifications are installed" do
+      it "installs instrumentation" do
+        Honeybadger::Dependency.inject!
+        expect(Net::HTTP.instance_method(:request)).to eq Net::HTTP.instance_method(:request_with_honeybadger)
       end
     end
-
-    it "installs instrumentation" do
-      Honeybadger::Dependency.inject!
-      expect(Net::HTTP.instance_method(:request)).to eq Net::HTTP.instance_method(:request_with_honeybadger)
+  else
+    context "when active support notifications are not installed" do
+      it "does not install instrumentation" do
+        Honeybadger::Dependency.inject!
+        expect(Net::HTTP.instance_methods).not_to include(:request_with_honeybadger)
+      end
     end
   end
 end
