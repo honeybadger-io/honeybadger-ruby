b677f51877cb2e8621ae8e1e29cc4ed3726ead0d
Joshua Wood <josh@hintmedia.com>
2014-09-24 10:31:22 -0700

    Add a comment about the debug backend.

--- lib/honeybadger/backend/debug.rb
+++ lib/honeybadger/backend/debug.rb
@@ -2,6 +2,9 @@ require 'honeybadger/backend/null'
 
 module Honeybadger
   module Backend
+    # Internal: Logs the notice payload rather than sending it. The purpose of
+    # this backend is primarily for programmatically inspecting JSON payloads
+    # in integration tests.
     class Debug < Null
       def notify(feature, payload)
         logger.unknown("notifying debug backend of feature=#{feature}\n\t#{payload.to_json}")
