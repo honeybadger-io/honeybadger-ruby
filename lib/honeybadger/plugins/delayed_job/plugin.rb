ba28758abb0b00990b968ea38a8c02ba9ac0fdf6
Panos Korros <panos@workable.com>
2015-02-02 19:30:09 +0200

    delayed_job improvements.
    Added component and action for delayed jobs.
    
    Set context before the delayed job is executed so that all faults reported with Honeybadger.notify have the correct context.
    
    Added support for setting the component and action for delayed_jobs that are not a Delayed::PerformableMethod but simple objects that response to :perform
    
    Include the component & action in the delayed job context so that all messages reported in this job have a proper component & action set.

--- lib/honeybadger/plugins/delayed_job/plugin.rb
+++ lib/honeybadger/plugins/delayed_job/plugin.rb
@@ -8,21 +8,34 @@ module Honeybadger
         callbacks do |lifecycle|
           lifecycle.around(:invoke_job) do |job, &block|
             begin
+              begin #buildin suport for Delayed::PerformableMethod
+                component = job.payload_object.object.is_a?(Class) ? job.payload_object.object.name : job.payload_object.object.class.name
+                action    = job.payload_object.method_name.to_s
+              rescue #fallback to support all other classes
+                component = job.payload_object.class.name
+                action    = 'perform'
+              end
+
+              ::Honeybadger.context(
+                :component     => component,
+                :action        => action,
+                :job_id        => job.id,
+                :handler       => job.handler,
+                :last_error    => job.last_error,
+                :attempts      => job.attempts,
+                :queue         => job.queue
+              )
+
               ::Honeybadger::Trace.instrument("#{job.payload_object.class}#perform", {source: 'delayed_job', jid: job.id, class: job.payload_object.class.name}) do
                 block.call(job)
               end
             rescue Exception => error
               ::Honeybadger.notify_or_ignore(
+                :component     => component,
+                :action        => action,
                 :error_class   => error.class.name,
                 :error_message => "#{ error.class.name }: #{ error.message }",
-                :backtrace     => error.backtrace,
-                  :context       => {
-                  :job_id        => job.id,
-                  :handler       => job.handler,
-                  :last_error    => job.last_error,
-                  :attempts      => job.attempts,
-                  :queue         => job.queue
-                }
+                :backtrace     => error.backtrace
               ) if job.attempts.to_i >= ::Honeybadger::Agent.config[:'delayed_job.attempt_threshold'].to_i
               raise error
             ensure
