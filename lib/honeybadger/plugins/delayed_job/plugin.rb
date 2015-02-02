0c01d331f9926303afd052fe80b5c7fac6673c5c
Panos Korros <panos@workable.com>
2015-02-02 19:32:35 +0200

    Set context before the delayed job is executed so that all faults reported with Honeybadger.notify have the correct context.

--- lib/honeybadger/plugins/delayed_job/plugin.rb
+++ lib/honeybadger/plugins/delayed_job/plugin.rb
@@ -8,6 +8,13 @@ module Honeybadger
         callbacks do |lifecycle|
           lifecycle.around(:invoke_job) do |job, &block|
             begin
+              ::Honeybadger.context(
+                :job_id        => job.id,
+                :handler       => job.handler,
+                :last_error    => job.last_error,
+                :attempts      => job.attempts,
+                :queue         => job.queue
+              )
               ::Honeybadger::Trace.instrument("#{job.payload_object.class}#perform", {source: 'delayed_job', jid: job.id, class: job.payload_object.class.name}) do
                 block.call(job)
               end
@@ -19,14 +26,7 @@ module Honeybadger
                 :action        => job.payload_object.method_name.to_s,
                 :error_class   => error.class.name,
                 :error_message => "#{ error.class.name }: #{ error.message }",
-                :backtrace     => error.backtrace,
-                  :context       => {
-                  :job_id        => job.id,
-                  :handler       => job.handler,
-                  :last_error    => job.last_error,
-                  :attempts      => job.attempts,
-                  :queue         => job.queue
-                }
+                :backtrace     => error.backtrace
               ) if job.attempts.to_i >= ::Honeybadger::Agent.config[:'delayed_job.attempt_threshold'].to_i
               raise error
             ensure
