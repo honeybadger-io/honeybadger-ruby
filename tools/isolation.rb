e6523c38e0bc3e213f5ba5b414b32809d1c9b482
Joshua Wood <josh@hintmedia.com>
2015-04-02 10:50:27 -0700

    Add a script to run specs in isolation.

---
+++ tools/isolation.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+
+catch :failure do
+  Dir['spec/unit/**/*_spec.rb'].each do |s|
+    Bundler.with_clean_env { puts `bundle exec rspec --pattern #{s}` }
+    throw :failure unless $?.exitstatus == 0
+  end
+end
