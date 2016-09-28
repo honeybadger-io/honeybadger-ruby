7a0edaa76c42b71ab229bd3d91befe2d94c4238f
Joshua Wood <josh@hintmedia.com>
2016-09-28 11:32:22 -0700

    List methods in NullWorker spec.

--- spec/unit/honeybadger/agent/null_worker_spec.rb
+++ spec/unit/honeybadger/agent/null_worker_spec.rb
@@ -2,7 +2,7 @@ require 'honeybadger/agent/worker'
 require 'honeybadger/agent/null_worker'
 
 describe Honeybadger::Agent::NullWorker do
-  Honeybadger::Agent::Worker.instance_methods.each do |method|
+  [:push, :shutdown, :shutdown!, :flush, :start].each do |method|
     it "responds to #{method}" do
       expect(subject).to respond_to(method)
       expect(subject.method(method).arity).to eq Honeybadger::Agent::Worker.instance_method(method).arity
--- spec/unit/honeybadger/agent/worker_spec.rb
+++ spec/unit/honeybadger/agent/worker_spec.rb
@@ -6,15 +6,6 @@ require 'honeybadger/config'
 require 'honeybadger/backend'
 require 'honeybadger/notice'
 
-describe Honeybadger::Agent::NullWorker do
-  Honeybadger::Agent::Worker.instance_methods.each do |method|
-    it "responds to #{method}" do
-      expect(subject).to respond_to(method)
-      expect(subject.method(method).arity).to eq Honeybadger::Agent::Worker.instance_method(method).arity
-    end
-  end
-end
-
 describe Honeybadger::Agent::Worker do
   let(:instance) { described_class.new(config, feature) }
   let(:config) { Honeybadger::Config.new(logger: NULL_LOGGER, debug: true, backend: 'null') }
