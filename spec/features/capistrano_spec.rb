ee509c7c47b37858de201461da8974c575e74a6c
Joshua Wood <josh@hintmedia.com>
2016-10-19 11:21:34 -0700

    Remove rails from features for now (covered in integrations).

--- spec/features/capistrano_spec.rb
+++ spec/features/capistrano_spec.rb
@@ -1,22 +1,10 @@
-feature "Installing honeybadger via the cli" do
-  shared_examples_for "capistrano deployment" do
-    before do
-      capify
-      append_to_file('Capfile', %(\nrequire 'capistrano/honeybadger'))
-    end
-
-    it "outputs the honeybadger task" do
-      expect(run('bundle exec cap -T')).to be_successfully_executed
-      expect(all_output).to match(/honeybadger\:deploy/i)
-    end
+feature "capistrano task" do
+  before do
+    FileUtils.cp(FIXTURES_PATH.join('Capfile'), current_dir)
   end
 
-  scenario "in a standalone project" do
-    it_behaves_like "capistrano deployment"
+  it "outputs the honeybadger task" do
+    expect(run('bundle exec cap -T')).to be_successfully_executed
+    expect(all_output).to match(/honeybadger\:deploy/i)
   end
-
-  scenario "in a Rails project", framework: :rails do
-    it_behaves_like "capistrano deployment"
-  end
-
 end
--- spec/features/rake_spec.rb
+++ spec/features/rake_spec.rb
@@ -1,58 +1,41 @@
 feature "Rescuing exceptions in a rake task" do
-  shared_examples_for "a rake application" do
+  before do
+    FileUtils.cp(FIXTURES_PATH.join('Rakefile'), current_dir)
+    set_environment_variable('HONEYBADGER_API_KEY', 'asdf')
+    set_environment_variable('HONEYBADGER_LOGGING_LEVEL', 'DEBUG')
+  end
+
+  it "reports the exception to Honeybadger" do
+    expect(run('rake honeybadger')).not_to be_successfully_executed
+    assert_notification('error' => {'class' => 'RuntimeError', 'message' => 'RuntimeError: Jim has left the building :('})
+  end
+
+  context "rake reporting is disabled" do
     before do
-      set_environment_variable('HONEYBADGER_API_KEY', 'asdf')
-      set_environment_variable('HONEYBADGER_LOGGING_LEVEL', 'DEBUG')
+      set_environment_variable('HONEYBADGER_EXCEPTIONS_RESCUE_RAKE', 'false')
     end
 
-    it "reports the exception to Honeybadger" do
+    it "doesn't report the exception to Honeybadger" do
       expect(run('rake honeybadger')).not_to be_successfully_executed
-      assert_notification('error' => {'class' => 'RuntimeError', 'message' => 'RuntimeError: Jim has left the building :('})
+      assert_no_notification
+    end
+  end
+
+  context "shell is attached" do
+    it "doesn't report the exception to Honeybadger" do
+      expect(run('rake honeybadger_autodetect_from_terminal')).not_to be_successfully_executed
+      assert_no_notification
     end
 
-    context "rake reporting is disabled" do
+    context "rake reporting is enabled" do
       before do
-        set_environment_variable('HONEYBADGER_EXCEPTIONS_RESCUE_RAKE', 'false')
+        set_environment_variable('HONEYBADGER_EXCEPTIONS_RESCUE_RAKE', 'true')
       end
 
-      it "doesn't report the exception to Honeybadger" do
-        expect(run('rake honeybadger')).not_to be_successfully_executed
-        assert_no_notification
-      end
-    end
-
-    context "shell is attached" do
-      it "doesn't report the exception to Honeybadger" do
+      it "reports the exception to Honeybadger" do
         expect(run('rake honeybadger_autodetect_from_terminal')).not_to be_successfully_executed
-        assert_no_notification
-      end
-
-      context "rake reporting is enabled" do
-        before do
-          set_environment_variable('HONEYBADGER_EXCEPTIONS_RESCUE_RAKE', 'true')
-        end
-
-        it "reports the exception to Honeybadger" do
-          expect(run('rake honeybadger_autodetect_from_terminal')).not_to be_successfully_executed
-          assert_notification('error' => {'class' => 'RuntimeError', 'message' => 'RuntimeError: Jim has left the building :('})
-        end
+        assert_notification('error' => {'class' => 'RuntimeError', 'message' => 'RuntimeError: Jim has left the building :('})
       end
     end
-  end
-
-  scenario "in a standalone project", framework: :ruby do
-    before do
-      FileUtils.cp(FIXTURES_PATH.join('Rakefile'), current_dir)
-    end
-
-    it_behaves_like "a rake application"
-  end
-
-  scenario "in a Rails project", framework: :rails do
-    before do
-      FileUtils.cp(FIXTURES_PATH.join('Rakefile'), RAILS_ROOT.join('lib/tasks/honeybadger.rake'))
-    end
-
-    it_behaves_like "a rake application"
   end
 end
