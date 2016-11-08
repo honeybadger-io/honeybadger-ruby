62dfce0859145057ea3cf308b9e32c65d26aa0ab
Joshua Wood <josh@honeybadger.io>
2016-11-08 14:58:19 -0800

    Restructure options.

--- spec/fixtures/ruby_custom.rb
+++ spec/fixtures/ruby_custom.rb
@@ -1,6 +1,11 @@
 require 'honeybadger/ruby'
 
-agent = Honeybadger::Agent.new(backend: 'debug', debug: true, api_key: 'asdf', logger: Logger.new(STDOUT))
+agent = Honeybadger::Agent.new({
+  api_key: 'asdf',
+  backend: 'debug',
+  debug: true,
+  logger: Logger.new(STDOUT)
+})
 
 agent.notify(error_class: 'CustomHoneybadgerException', error_message: 'Test message')
 
