57b59be3864bd4000d9278257f98e2a0114db9a4
Joshua Wood <josh@hintmedia.com>
2016-11-15 21:48:43 -0800

    Refactor Yaml config source.

--- lib/honeybadger/config/yaml.rb
+++ lib/honeybadger/config/yaml.rb
@@ -4,40 +4,39 @@ require 'erb'
 
 module Honeybadger
   class Config
-    class Yaml < ::Hash
+    module Yaml
       DISALLOWED_KEYS = [:'config.path'].freeze
 
-      def initialize(path, env = 'production')
-        @path = path.kind_of?(Pathname) ? path : Pathname.new(path)
+      def self.new(path, env = 'production')
+        path = path.kind_of?(Pathname) ? path : Pathname.new(path)
 
-        if !@path.exist?
-          raise ConfigError, "The configuration file #{@path} was not found."
-        elsif !@path.file?
-          raise ConfigError, "The configuration file #{@path} is not a file."
-        elsif !@path.readable?
-          raise ConfigError, "The configuration file #{@path} is not readable."
-        else
-          yaml = load_yaml
-          yaml.merge!(yaml[env]) if yaml[env].kind_of?(Hash)
-          update(dotify_keys(yaml))
+        if !path.exist?
+          raise ConfigError, "The configuration file #{path} was not found."
+        elsif !path.file?
+          raise ConfigError, "The configuration file #{path} is not a file."
+        elsif !path.readable?
+          raise ConfigError, "The configuration file #{path} is not readable."
         end
+
+        yaml = load_yaml(path)
+        yaml.merge!(yaml[env]) if yaml[env].kind_of?(Hash)
+
+        dotify_keys(yaml)
       end
 
-      private
-
-      def load_yaml
-        yaml = YAML.load(ERB.new(@path.read).result)
+      def self.load_yaml(path)
+        yaml = YAML.load(ERB.new(path.read).result)
         case yaml
         when Hash
           yaml
         when NilClass, FalseClass
           {}
         else
-          raise ConfigError, "The configuration file #{@path} is invalid."
+          raise ConfigError, "The configuration file #{path} is invalid."
         end
       end
 
-      def dotify_keys(hash, key_prefix = nil)
+      def self.dotify_keys(hash, key_prefix = nil)
         {}.tap do |new_hash|
           hash.each_pair do |k,v|
             k = [key_prefix, k].compact.join('.')
