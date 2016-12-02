ad41ad2425b94ce07dc51e0886b50f38eaff35d0
Joshua Wood <josh@hintmedia.com>
2016-12-02 09:55:47 -0800

    Whitelist CGI variables.

--- spec/unit/honeybadger/util/request_hash_spec.rb
+++ spec/unit/honeybadger/util/request_hash_spec.rb
@@ -6,9 +6,14 @@ describe Honeybadger::Util::RequestHash, if: defined?(Rack) do
   subject { described_class.from_env(rack_env) }
 
   describe "cgi_data" do
-    it "includes UPPER_CASE_KEY" do
-      rack_env['UPPER_CASE_KEY'] = 'foo'
-      expect(subject[:cgi_data]['UPPER_CASE_KEY']).to eq 'foo'
+    it "includes all HTTP headers" do
+      rack_env['HTTP_SOME_RANDOM_HEADER'] = 'foo'
+      expect(subject[:cgi_data]['HTTP_SOME_RANDOM_HEADER']).to eq 'foo'
+    end
+
+    it "includes approved CGI variables" do
+      rack_env['REMOTE_ADDR'] = '127.0.0.1'
+      expect(subject[:cgi_data]['REMOTE_ADDR']).to eq '127.0.0.1'
     end
 
     it "excludes RAW_POST_DATA" do
