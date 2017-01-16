8fdf1d1c57ae0755eb4a055b3d68826692c16536
Joshua Wood <josh@honeybadger.io>
2017-01-16 09:00:03 -0800

    Update allocation_stats traceer.

--- tools/allocation_stats.rb
+++ tools/allocation_stats.rb
@@ -10,15 +10,18 @@ group_by = if ENV['GROUP']
 
 puts Benchmark.measure {
   stats = AllocationStats.trace do
-    if Honeybadger.start({:api_key => 'badgers', :backend => 'null'})
-      1000.times do
-        Honeybadger.notify(error_class: 'RubyProf', error_message: 'Profiling Honeybadger -- this should never actually be reported.')
-      end
+    Honeybadger.configure do |config|
+      config.api_key = 'badgers'
+      config.backend = 'null'
+    end
+
+    1000.times do
+      Honeybadger.notify(error_class: 'RubyProf', error_message: 'Profiling Honeybadger -- this should never actually be reported.')
     end
   end
 
-  Honeybadger::Agent.at_exit do
-    puts "\n\n"
-    puts stats.allocations(alias_paths: true).group_by(*group_by).to_text
-  end
+  Honeybadger.flush
+
+  puts "\n\n"
+  puts stats.allocations(alias_paths: true).group_by(*group_by).to_text
 }
