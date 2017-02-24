c0b112dc91243e0dd0b6a55b69efb8f9fe1a165e
Pablo Cantero <pablo@pablocantero.com>
2017-02-23 20:24:38 -0500

    Add a specific spec for Shoryuken batch

--- spec/unit/honeybadger/plugins/shoryuken_spec.rb
+++ spec/unit/honeybadger/plugins/shoryuken_spec.rb
@@ -48,11 +48,27 @@ RSpec.describe TestShoryukenWorker do
            attributes: { "ApproximateReceiveCount" => receive_count },
            data: double("SqsMsgData", message_id: rand.to_s))
   end
+  let(:body) { { "key" => "value" } }
 
   shared_examples_for "notifies Honeybadger" do
     it do
       expect(Honeybadger).to receive(:notify).with(kind_of(RuntimeError),
                                                    hash_including(parameters: { body: { "key" => "value" } }))
+
+      expect { job_execution }.to raise_error(RuntimeError)
+    end
+  end
+
+  shared_examples_for "batch notifies Honeybadger" do
+    it do
+      expect(Honeybadger).to receive(:notify).with(kind_of(RuntimeError),
+                                                   hash_including(parameters:
+                                                                    { batch: [
+                                                                        { "key" => "value" },
+                                                                        { "key" => "value" }
+                                                                      ]
+                                                                    }
+                                                                 ))
 
       expect { job_execution }.to raise_error(RuntimeError)
     end
@@ -67,10 +83,10 @@ RSpec.describe TestShoryukenWorker do
 
   let(:receive_count) { "1" }
   let(:sqs_msgs) { sqs_msg }
+  let(:bodies) { body }
   let(:instance) { described_class.new }
-  let(:body) { { "key" => "value" } }
   let(:job_execution) do
-    instance.call(instance, nil, sqs_msgs, body) { raise "foo" }
+    instance.call(instance, nil, sqs_msgs, bodies) { raise "foo" }
   end
 
   context "with a single message" do
@@ -93,6 +109,8 @@ RSpec.describe TestShoryukenWorker do
 
   context "with several messages" do
     let(:sqs_msgs) { Array.new(2) { sqs_msg.dup } }
-    include_examples "notifies Honeybadger"
+    let(:bodies) { Array.new(2) { body.dup } }
+
+    include_examples "batch notifies Honeybadger"
   end
 end
