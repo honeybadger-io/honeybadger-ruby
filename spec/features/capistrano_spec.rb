ebe19f7393eaf536786f0e3eb44bbec7c5884725
Joshua Wood <josh@joshuawood.net>
2018-01-15 16:13:37 -0800

    Add debug output.

--- spec/features/capistrano_spec.rb
+++ spec/features/capistrano_spec.rb
@@ -3,6 +3,15 @@ feature "capistrano task" do
     FileUtils.cp(FIXTURES_PATH.join('Capfile'), current_dir)
   end
 
+  after do
+    puts '*' * 100
+    puts 'PATH:'
+    puts ENV['PATH']
+    puts 'output:'
+    puts all_output
+    puts '*' * 100
+  end
+
   it "outputs the honeybadger task" do
     expect(run('bundle exec cap -T')).to be_successfully_executed
     expect(all_output).to match(/honeybadger\:deploy/i)
