446932efc7c9ca599dc6730cf6726d82213000ea
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-01-24 19:10:50 +0530

    Fix at_exit callback being skipped in a rails app with sinatra
    When a rails app had a dependency on sinatra (imported in gems like resque), the
    at_exit callback wasn't being set up at all. This fixes it by explicitly
    setting up the at_exit callbacks in rails and ruby init scripts

--- lib/honeybadger.rb
+++ lib/honeybadger.rb
@@ -9,10 +9,3 @@ end
 if defined?(Rake.application)
   require 'honeybadger/init/rake'
 end
-
-# Sinatra is a special case. Sinatra starts the web application in an at_exit
-# handler. And, since we require sinatra before requiring HB, the only way to
-# setup our at_exit callback is in the sinatra build callback honeybadger/init/sinatra.rb
-if !defined?(Sinatra::Base)
-  Honeybadger.install_at_exit_callback
-end
