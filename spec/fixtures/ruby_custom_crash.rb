bcf18e1cc25ab11dc4f124718bbd6026cedaad8e
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-03-22 09:09:31 +0530

    Fix failing test caused by missing signal normalization

--- spec/fixtures/ruby_custom_crash.rb
+++ spec/fixtures/ruby_custom_crash.rb
@@ -3,7 +3,7 @@ require 'honeybadger'
 CRASHES = {
   "system_exit" => ->{ exit -1 },
   "sigterm" => ->{ raise SignalException, "TERM" },
-  "hup" => ->{ raise SignalException, "HUP" },
+  "hup" => ->{ raise SignalException, "SIGHUP" },
 }
 
 crash_type = ARGV.first || (raise "Invalid argument")
