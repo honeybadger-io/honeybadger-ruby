15dd785f664d4f00d7a9992d607b448f5b7da595
gkent <gkent@invoca.com>
2018-08-31 18:12:16 -0700

    Add flag to optionally skip rails initialization for cli commands

--- spec/features/notify_spec.rb
+++ spec/features/notify_spec.rb
@@ -31,4 +31,32 @@ feature "Running the notify CLI command" do
       assert_notification('api_key' => 'my-key')
     end
   end
+
+  context "when Rails is not detected due to a missing environment.rb" do
+    it "skips rails initialization without logging" do
+      expect(run('honeybadger notify --message "Test error message" --skip-rails-load')).to be_successfully_executed
+      expect(all_output).to_not match(/Skipping Rails initialization/i)
+    end
+  end
+
+  context "when Rails is detected via the presence of environment.rb" do
+    before do
+      config_path = File.join(Dir.pwd, 'tmp', 'features', 'config')
+      Dir.mkdir(config_path) unless File.exists?(config_path)
+      File.open(File.join(config_path, 'environment.rb'), 'w')
+    end
+
+    it "skips rails initialization when true" do
+      expect(run('honeybadger notify --message "Test error message" --skip-rails-load')).to be_successfully_executed
+      expect(all_output).to match(/Skipping Rails initialization/i)
+    end
+
+    it "does not skip rails initialization when false or not set" do
+      expect(run('honeybadger notify --message "Test error message" --skip-rails-load=false')).to be_successfully_executed
+      expect(all_output).to_not match(/Skipping Rails initialization/i)
+
+      expect(run('honeybadger notify --message "Test error message"')).to be_successfully_executed
+      expect(all_output).to_not match(/Skipping Rails initialization/i)
+    end
+  end
 end
