bab4ca522fa2cc2e4e022ad1fcdf11b5bed4afc0
Kevin Webster <kevin@kevinwebster.me>
2019-05-15 11:38:01 -0700

    Basic ring buffer

---
+++ lib/honeybadger/breadcrumb/ring_buffer.rb
@@ -0,0 +1,21 @@
+module Breadcrumb
+  class RingBuffer
+    attr_reader :buffer
+
+    def initialize(buffer_size = 40)
+      @buffer_size = buffer_size
+      clear!
+    end
+
+    def add!(item)
+      @buffer << item
+      @ct += 1
+      @buffer.shift(1) if @ct > @buffer_size
+    end
+
+    def clear!
+      @buffer = []
+      @ct = 0
+    end
+  end
+end
---
+++ spec/unit/honeybadger/breadcrumb/ring_buffer_spec.rb
@@ -0,0 +1,27 @@
+require 'honeybadger/breadcrumb/ring_buffer'
+
+describe Breadcrumb::RingBuffer do
+  describe "#add" do
+    it 'adds items' do
+      subject.add!(:a)
+      expect(subject.buffer).to eq([:a])
+    end
+
+    it 'shifts when size limit is hit' do
+      buffer = described_class.new(2)
+      buffer.add!(:a)
+      buffer.add!(:b)
+      buffer.add!(:c)
+
+      expect(buffer.buffer).to eq([:b, :c])
+    end
+  end
+
+  describe "#clear" do
+    it 'clears data' do
+      subject.add!(:a)
+      subject.clear!
+      expect(subject.buffer).to be_empty
+    end
+  end
+end
