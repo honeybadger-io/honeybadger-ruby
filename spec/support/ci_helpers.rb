154e115e1606adcd4598236d39976d03122f29f7
Kevin Webster <kevin@kevinwebster.me>
2019-10-14 20:09:30 -0700

    Lets try this

---
+++ spec/support/ci_helpers.rb
@@ -0,0 +1,14 @@
+class CIHelpers
+  def self.results_name
+    [
+      ENV['TEST_OUTPUT_NAME']
+      gemfile
+    ].compact.join(" - ")
+  end
+
+  def self.gemfile
+    return nil if ENV['BUNDLE_GEMFILE'].nil?
+
+    ENV['BUNDLE_GEMFILE'].split(/\//).last
+  end
+end
