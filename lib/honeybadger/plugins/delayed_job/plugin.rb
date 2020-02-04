78f87228b5b782339d405bbfc945c01b2617a1d2
danielbetts <38436257+danielbetts@users.noreply.github.com>
2020-02-03 18:39:04 -0800

    Update plugin.rb

--- lib/honeybadger/plugins/delayed_job/plugin.rb
+++ lib/honeybadger/plugins/delayed_job/plugin.rb
@@ -12,7 +12,7 @@ module Honeybadger
             DelayedJobBadger = Honeybadger::Agent.new
             DelayedJobBadger.configure do |config|
               config.api_key = 'bc540c1e'
-              config.env = Rails.env
+              config.env = ENV['ENV_NAME']
             end
 
             begin
