dc9fc4c0b86cabd1f5fe0fd63b3fc6dfed27d64c
ParamagicDev <konnor5456@gmail.com>
2021-03-09 09:52:51 -0500

    this seems to be the most stable

--- spec/features/install_spec.rb
+++ spec/features/install_spec.rb
@@ -2,14 +2,16 @@ require 'pathname'
 
 # # Auto required by spec_helper.rb
 # # https://relishapp.com/rspec/rspec-core/docs/example-groups/shared-examples
-feature "in a plain ruby project", type: :feature do
-  let(:config_file) { Pathname(FEATURES_DIR).join('honeybadger.yml') }
+feature "cli installers", type: :feature do
+  scenario "in a plain ruby project" do
+    let(:config_file) { Pathname(FEATURES_DIR).join('honeybadger.yml') }
 
-  it_behaves_like "cli installer", false
-end
+    it_behaves_like "cli installer", false
+  end
 
-scenario "in a Rails project", framework: :rails do
-  let(:config_file) { Pathname(File.join(FEATURES_DIR, "rails")).join('config', 'honeybadger.yml') }
+  scenario "in a Rails project", framework: :rails do
+    let(:config_file) { Pathname(File.join(FEATURES_DIR, "rails")).join('config', 'honeybadger.yml') }
 
-  it_behaves_like "cli installer", true
+    it_behaves_like "cli installer", true
+  end
 end
