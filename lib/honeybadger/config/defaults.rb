ec830400cec371fc7dfc55a66acc78699f79fbca
shalvah <diakon.ng@gmail.com>
2022-08-10 00:51:44 +0200

    ErrorSubscriber: Don't ignore cache errors by default

--- lib/honeybadger/config/defaults.rb
+++ lib/honeybadger/config/defaults.rb
@@ -301,8 +301,8 @@ module Honeybadger
         type: Boolean
       },
       :'rails.subscriber_ignore_sources' => {
-        description: "Sources that should be ignored when using the Rails' (7+) native error reporter.",
-        default: [/\w+_cache_store.active_support/],
+        description: "Sources (strings or regexes) that should be ignored when using the Rails' (7+) native error reporter.",
+        default: [],
         type: Array
       },
       :'resque.resque_retry.send_exceptions_when_retrying' => {
