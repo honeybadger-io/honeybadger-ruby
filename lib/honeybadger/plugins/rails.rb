bfe580d21456e58a1e54c55424931f9879e5ef5d
Joshua Wood <josh@joshuawood.net>
2022-08-15 11:54:51 -0700

    Update lib/honeybadger/plugins/rails.rb

--- lib/honeybadger/plugins/rails.rb
+++ lib/honeybadger/plugins/rails.rb
@@ -33,7 +33,7 @@ module Honeybadger
         def self.report(exception, handled:, severity:, context: {}, source: nil)
           return if source && ::Honeybadger.config[:'rails.subscriber_ignore_sources'].any? { |regex| regex.match?(source) }
 
-          tags = ['reporter:rails.error_subscriber', "severity:#{severity}", "handled:#{handled}"]
+          tags = ["severity:#{severity}"]
           tags << "source:#{source}" if source
           Honeybadger.notify(exception, context: context, tags: tags)
           exception.instance_variable_set(:@__hb_handled, true)
