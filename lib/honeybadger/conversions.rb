408c1fbaae104e2d479ab7e23bab693aa43e01f5
Dan Leyden <dan@52degreesnorth.com>
2023-08-10 10:17:14 +0100

    Add support for reporting nested context
    When providing an object to the context that comprises other objects
    that implement `to_honeybadger_context`, serialise them using the using
    their `to_honeybadger_context` rather than a simple `to_json`

--- lib/honeybadger/conversions.rb
+++ lib/honeybadger/conversions.rb
@@ -2,15 +2,24 @@ module Honeybadger
   # @api private
   module Conversions
     module_function
+    MAX_CONTEXT_DEPTH = 20
 
     # Convert context into a Hash.
     #
     # @param [Object] object The context object.
     #
     # @return [Hash] The hash context.
-    def Context(object)
+    def Context(object, depth = 1)
       object = object.to_honeybadger_context if object.respond_to?(:to_honeybadger_context)
-      Hash(object)
+      object = Hash(object)
+      object = object.transform_values do |value|
+        if value&.respond_to?(:to_honeybadger_context)
+          Context(value, depth + 1)
+        else
+          value
+        end
+      end if depth < MAX_CONTEXT_DEPTH
+      object
     end
   end
 end
