8b148da59beb9b09685d0ddf899f7dbf3820915f
Roel Bondoc <rsbondoc@gmail.com>
2023-08-17 23:57:23 -0400

    Add subscription to "honeybadger" notifications

--- lib/honeybadger/plugins/auto_logging.rb
+++ lib/honeybadger/plugins/auto_logging.rb
@@ -7,12 +7,18 @@ module Honeybadger
       requirement { config[:'auto_logging.enabled'] }
 
       execution do
-        next if @subscriber
+        next if @auto_logging_loaded
 
-        @subscriber = ActiveSupport::Notifications.subscribe("process_action.action_controller") do |name, started, finished, unique_id, payload|
+        ActiveSupport::Notifications.subscribe("process_action.action_controller") do |name, started, finished, unique_id, payload|
           Honeybadger::Logger.info(payload.slice(:controller, :action, :params, :format,
             :method, :path, :status, :view_runtime, :db_runtime).merge({ event: name }))
         end
+
+        ActiveSupport::Notifications.subscribe(/honeybadger/) do |name, started, finished, unique_id, payload|
+          Honeybadger::Logger.info(payload.merge({ event: name }))
+        end
+
+        @auto_logging_loaded = true
       end
     end
   end
