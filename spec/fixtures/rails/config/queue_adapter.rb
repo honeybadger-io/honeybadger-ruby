99abc5d2e91983657155fbc21f83a9188d5311c6
Jan Krutisch <jan@krutisch.de>
2023-10-27 15:40:12 +0200

    Test with around filter

--- spec/fixtures/rails/config/queue_adapter.rb
+++ spec/fixtures/rails/config/queue_adapter.rb
@@ -1,6 +1,12 @@
 class ErrorJob < ActiveJob::Base
+  around_perform :around_for_testing
+
   def perform(opts={})
     raise "exception raised in job"
+  end
+
+  def around_for_testing(*args)
+    yield
   end
 end
 
