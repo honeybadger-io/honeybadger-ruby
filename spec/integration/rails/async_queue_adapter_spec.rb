899e4d72d4b0f77d513018852556a7e8f0434379
Jan Krutisch <jan@krutisch.de>
2023-10-27 13:06:20 +0200

    Restructure test for older rails versions

--- spec/integration/rails/async_queue_adapter_spec.rb
+++ spec/integration/rails/async_queue_adapter_spec.rb
@@ -1,18 +1,16 @@
 require_relative '../rails_helper'
 
 describe "Rails Async Queue Adapter Test", if: RAILS_PRESENT, type: :request do
+  include ActiveJob::TestHelper
   load_rails_hooks(self)
 
   it "reports exceptions" do
-    include ActiveJob::TestHelper
-    
     Honeybadger.flush do
-      post "/enqueue_error_job"
-      expect(response.status).to eq(200)
+      perform_enqueued_jobs do
+        post "/enqueue_error_job"
+      end
+      expect(response.status).to eq(500)
     end
-
-    expect { perform_enqueued_jobs }.to raise_error(RuntimeError)
-
     expect(Honeybadger::Backend::Test.notifications[:notices].size).to eq(1)
     expect(Honeybadger::Backend::Test.notifications[:notices][0].params[:job_arguments][0]).to eq({some: 'data'})
   end
