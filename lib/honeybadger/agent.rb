2097dbfd42f2ac92694fa9049d4ebf7f3ad428fc
Jan Krutisch <jan@krutisch.de>
2024-01-05 12:23:03 +0100

    fix #event: Use Hash() and reverse merge order.

--- lib/honeybadger/agent.rb
+++ lib/honeybadger/agent.rb
@@ -371,12 +371,8 @@ module Honeybadger
     # @param payload [Hash] Additional data to be sent with the event as keyword arguments
     def event(event_type, payload={})
       ts = DateTime.now.new_offset(0).rfc3339
-      unless payload.kind_of?(Hash)
-        logger.error("Event has non-hash payload")
-        return
-      end
-      log_string = payload.merge({event_type: event_type, ts: ts}).to_json
-      logger.debug(log_string)
+      merged = {event_type: event_type, ts: ts}.merge(Hash(payload))
+      logger.debug(merged.to_json)
     end
 
     # @api private
