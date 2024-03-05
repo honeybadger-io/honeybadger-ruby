4e2d4287bbbe0100d6f82a38b7314fc8dc5a1571
Benjamin Curtis <ben@bencurtis.com>
2024-03-05 09:04:35 -0800

    feat: track exceptions in :solid_queue (#526)
    And any other ActiveJob adapters that come along :)
    
    Fixes #518

--- lib/honeybadger/plugins/active_job.rb
+++ lib/honeybadger/plugins/active_job.rb
@@ -1,29 +1,30 @@
 module Honeybadger
   module Plugins
     module ActiveJob
-            
+      # Ignore inline and test adapters, as well as the adapters that we support with their own plugins
+      EXCLUDED_ADAPTERS = %i[inline test delayed_job faktory karafka resque shoryuken sidekiq sucker_punch].freeze
+
       Plugin.register {
         requirement { defined?(::Rails.application) && ::Rails.application }
         requirement {
           ::Rails.application.config.respond_to?(:active_job) &&
-            ::Rails.application.config.active_job[:queue_adapter] == :async
+            !EXCLUDED_ADAPTERS.include?(::Rails.application.config.active_job[:queue_adapter])
         }
-        
+
         execution {
-          ::ActiveJob::Base.class_eval do |base| 
+          ::ActiveJob::Base.class_eval do |base|
             base.set_callback :perform, :around do |param, block|
               Honeybadger.clear!
               begin
                 block.call
               rescue => error
-                Honeybadger.notify(error, parameters: { job_arguments: self.arguments })
+                Honeybadger.notify(error, parameters: {job_id: job_id, arguments: arguments})
                 raise error
               end
             end
-          end          
+          end
         }
       }
     end
   end
-
 end
