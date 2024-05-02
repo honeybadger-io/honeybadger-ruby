d7e4a228e91f3a83152c5c02bf29855e3d2c04e8
Roel Bondoc <rsbondoc@gmail.com>
2024-05-02 11:18:50 -0400

    Add Honeybadger::Instrumentation class
    This adds a `Honeybadger::Instrumentation` class to help facilitate
    gathering metrics within the gem. It's used by calling class methods for
    the various types of metrics.
    
    The `Honeybadger::InstrumentationHelper` module can be included into any
    class for a cleaner metric DSL.
    
    For this round of implementaiton, the metrics are simply sent off as
    events to Insights for collection.

---
+++ lib/honeybadger/instrumentation.rb
@@ -0,0 +1,79 @@
+module Honeybadger
+  class Instrumentation
+    def self.monotonic_timer
+      start_time = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+      result = yield
+      finish_time = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+      [((finish_time - start_time) * 1000).round(2), result]
+    end
+
+    def self.histogram(name, attributes: {}, duration: nil)
+      if block_given?
+        duration = monotonic_timer{ yield }[0]
+      end
+
+      raise 'No duration found' if duration.nil?
+
+      attributes.merge!(metric_type: "histogram", metric_name: name)
+      Honeybadger.event(event_type: "hb.metrics", duration: duration, **attributes)
+    end
+
+    def self.increment_counter(name, count: 1, attributes: {})
+      attributes.merge!(metric_type: "counter", metric_name: name)
+      record(count: count, **attributes)
+    end
+
+    def self.gauge(name, value:, attributes: {})
+      attributes.merge!(metric_type: "gauge", metric_name: name)
+      record(value: value, **attributes)
+    end
+
+    def self.record(args)
+      Honeybadger.event(args.merge(event_type: "hb.metrics", hostname: Honeybadger.config[:hostname].to_s))
+    end
+  end
+
+  module InstrumentationHelper
+    def monotonic_timer
+      Honeybadger::Instrumentation.monotonic_timer { yield }
+    end
+
+    def metric_source(source)
+      @metric_source = source
+    end
+
+    def metric_attributes(attributes)
+      raise "metric_attributes expects a hash" unless attributes.is_a?(Hash)
+      @metric_attributes = attributes
+    end
+
+    def histogram(name, *args)
+      attributes = extract_attributes(args)
+      body = args.select { |a| a.respond_to?(:call) }.first
+      if body
+        Honeybadger::Instrumentation.histogram(name, attributes: attributes) { body.call }
+      elsif attributes.keys.include?(:duration)
+        Honeybadger::Instrumentation.histogram(name, attributes: attributes, duration: attributes.delete(:duration))
+      end
+    end
+
+    def increment_counter(name, *args)
+      attributes = extract_attributes(args)
+      count = args.select { |a| a.respond_to?(:call) }.first&.call || 1
+      Honeybadger::Instrumentation.increment_counter(name, count: count, attributes: attributes)
+    end
+
+    def gauge(name, *args)
+      attributes = extract_attributes(args)
+      value = args.select { |a| a.respond_to?(:call) }.first.call
+      Honeybadger::Instrumentation.gauge(name, value: value, attributes: attributes)
+    end
+
+    def extract_attributes(args)
+      @metric_attributes ||= {}
+      attributes = args.select { |a| a.is_a?(Hash) }.first || {}
+      attributes.merge(metric_source: @metric_source).merge(@metric_attributes).compact
+    end
+  end
+end
+
