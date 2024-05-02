b15d02ec5f9d1b765ec642dbcaff3419027f37b6
Roel Bondoc <rsbondoc@gmail.com>
2024-05-02 11:39:19 -0400

    Add a Puma plugin for Honeybadger
    This adds a Puma plugin for Honeybadger.
    
    Metrics on the puma process are gathered in a background thread every 1
    second and should work for both single threaded and clustered mode.

---
+++ lib/puma/plugin/honeybadger.rb
@@ -0,0 +1,43 @@
+require 'honeybadger/instrumentation'
+
+module Honeybadger
+  class PumaPlugin
+    include Honeybadger::InstrumentationHelper
+
+    ::Puma::Plugin.create do
+      def start(launcher)
+        puma_plugin = ::Honeybadger::PumaPlugin.new
+        in_background do
+          loop do
+            puma_plugin.record
+            sleep 1
+          end
+        end
+      end
+    end
+
+    def record
+      metric_source 'puma'
+
+      stats = ::Puma.stats rescue {}
+      stats = stats.is_a?(Hash) ? stats : JSON.parse(stats, symbolize_names: true)
+
+      if stats[:worker_status].is_a?(Array)
+        stats[:worker_status].each do |worker_data|
+          context = { index: worker_data[:index] }
+          record_puma_stats(worker_data[:last_status], context)
+        end
+      else
+        record_puma_stats(stats)
+      end
+    end
+
+    def record_puma_stats(stats, context={})
+      gauge "pool_capacity", context, ->{ stats[:pool_capacity] }
+      gauge "max_threads", context, ->{ stats[:max_threads] }
+      gauge "requests_count", context, ->{ stats[:requests_count] }
+      gauge "backlog", context, ->{ stats[:backlog] }
+      gauge "running", context, ->{ stats[:running] }
+    end
+  end
+end
