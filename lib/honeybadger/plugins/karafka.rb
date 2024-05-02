ff5990a865b42c43ef077c0329f5503366b6e45a
Roel Bondoc <rsbondoc@gmail.com>
2024-05-02 11:38:18 -0400

    Add Insights events for karafka

--- lib/honeybadger/plugins/karafka.rb
+++ lib/honeybadger/plugins/karafka.rb
@@ -1,14 +1,29 @@
 require 'honeybadger/plugin'
-require 'honeybadger/ruby'
 
 module Honeybadger
   module Plugins
-    Plugin.register do
+    Plugin.register :karafka do
       requirement { defined?(::Karafka) }
 
       execution do
         ::Karafka.monitor.subscribe('error.occurred') do |event|
           Honeybadger.notify(event[:error])
+          Honeybadger.event('error.occurred', error: event[:error]) if config.load_plugin_insights?(:karafka)
+        end
+
+        if config.load_plugin_insights?(:karafka)
+          ::Karafka.monitor.subscribe("consumer.consumed") do |event|
+            context = {
+              duration: event.payload[:time],
+              consumer: event.payload[:caller].class.to_s,
+              id: event.payload[:caller].id,
+              topic: event.payload[:caller].messages.metadata.topic,
+              messages_count: event.payload[:caller].messages.metadata.size,
+              partition: event.payload[:caller].messages.metadata.partition
+            }
+
+            Honeybadger.event('consumer.consumed', context)
+          end
         end
       end
     end
