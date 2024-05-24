36530ec0bf54b7d5e3fdd552622c06b120ceef11
Roel Bondoc <rsbondoc@gmail.com>
2024-05-24 16:28:07 -0400

    Add specs for InstrumentationHelper module

--- lib/honeybadger/histogram.rb
+++ lib/honeybadger/histogram.rb
@@ -2,8 +2,8 @@ require 'honeybadger/metric'
 
 module Honeybadger
   class Histogram < Metric
-    DEFAULT_BINS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10]
-    INFINITY = 1e20 # not quite, but pretty much
+    DEFAULT_BINS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0]
+    INFINITY = 1e20.to_f # not quite, but pretty much
 
     def record(value)
       return unless value
