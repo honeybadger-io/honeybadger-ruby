0fc7e36c38df901cf25f0fb3369e4d1a16e6d244
Roel Bondoc <rsbondoc@gmail.com>
2024-06-20 11:31:56 -0400

    Add a comment for flat_hash

--- lib/honeybadger/agent.rb
+++ lib/honeybadger/agent.rb
@@ -601,6 +601,9 @@ module Honeybadger
       error { "Rescued an error in a before hook: #{ex.message}" }
     end
 
+    # Converts a nested hash into a single layer where keys become arrays:
+    # ex: > flat_hash({ :nested => { :hash => "value" }})
+    #     > { [:nested, :hash] => "value" }
     def flat_hash(h,f=[],g={})
       return g.update({ f=>h }) unless h.is_a? Hash
       h.each { |k,r| flat_hash(r,f+[k],g) }
