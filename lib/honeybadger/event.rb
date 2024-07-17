9f1d6b55e88497c4c37659fdfaeaa163c7794672
Benjamin Curtis <ben@bencurtis.com>
2024-07-17 11:11:16 -0700

    fix: add []= delegator (#590)
    This allows code that updates the payload in a before_event block to look a little nicer. :)

--- lib/honeybadger/event.rb
+++ lib/honeybadger/event.rb
@@ -13,7 +13,7 @@ module Honeybadger
     # The payload data of the event
     attr_reader :payload
 
-    def_delegator :payload, :[]
+    def_delegators :payload, :[], :[]=
 
     # @api private
     def initialize(event_type_or_payload, payload={})
