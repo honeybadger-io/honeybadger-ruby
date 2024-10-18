c815397b67aaf2b5cfa5c39f646583ccc4c3a4e0
Roel Bondoc <rsbondoc@gmail.com>
2024-10-18 14:01:29 -0400

    Also subscribe to producer errors

--- lib/honeybadger/plugins/karafka.rb
+++ lib/honeybadger/plugins/karafka.rb
@@ -7,7 +7,9 @@ module Honeybadger
       requirement { defined?(::Karafka) && ::Karafka.respond_to?(:monitor) }
 
       execution do
-        ::Karafka.monitor.subscribe(::Honeybadger::Karafka::ErrorsListener.new)
+        errors_listener = ::Honeybadger::Karafka::ErrorsListener.new
+        ::Karafka.monitor.subscribe(errors_listener)
+        ::Karafka.producer.monitor.subscribe(errors_listener) if ::Karafka.respond_to?(:producer)
 
         if config.load_plugin_insights?(:karafka)
           ::Karafka.monitor.subscribe(::Honeybadger::Karafka::InsightsListener.new)
