96baff17608b30ba6d982f40c3b801004e32d9b4
Roel Bondoc <rsbondoc@gmail.com>
2024-11-08 10:37:39 -0500

    Add separate context for enabling metrics

--- spec/unit/honeybadger/plugins/net_http_spec.rb
+++ spec/unit/honeybadger/plugins/net_http_spec.rb
@@ -19,7 +19,6 @@ describe "Net::HTTP integration" do
     context "report domain only" do
       it "contains a domain" do
         expect(Honeybadger).to receive(:event).with('request.net_http', hash_including({method: "GET", status: 200, host: "example.com"}))
-        expect(Honeybadger).to receive(:gauge).with('duration.request', hash_including({method: "GET", status: 200, host: "example.com"}))
         Net::HTTP.get(URI.parse('http://example.com'))
       end
     end
@@ -29,8 +28,29 @@ describe "Net::HTTP integration" do
 
       it "contains a domain and url" do
         expect(Honeybadger).to receive(:event).with('request.net_http', hash_including({method: "GET", status: 200, url: "http://example.com", host: "example.com"}))
-        expect(Honeybadger).to receive(:gauge).with('duration.request', hash_including({method: "GET", status: 200, host: "example.com"}))
         Net::HTTP.get(URI.parse('http://example.com'))
+      end
+    end
+
+    context "metrics collection enabled" do
+      let(:config) { Honeybadger::Config.new(logger: NULL_LOGGER, debug: true, :'insights.enabled' => true, :'net_http.insights.metrics' => true) }
+
+      context "report domain only" do
+        it "contains a domain" do
+          expect(Honeybadger).to receive(:event).with('request.net_http', hash_including({method: "GET", status: 200, host: "example.com"}))
+          expect(Honeybadger).to receive(:gauge).with('duration.request', hash_including({method: "GET", status: 200, host: "example.com"}))
+          Net::HTTP.get(URI.parse('http://example.com'))
+        end
+      end
+
+      context "report domain and full url" do
+        before { config[:'net_http.insights.full_url'] = true }
+
+        it "contains a domain and url" do
+          expect(Honeybadger).to receive(:event).with('request.net_http', hash_including({method: "GET", status: 200, url: "http://example.com", host: "example.com"}))
+          expect(Honeybadger).to receive(:gauge).with('duration.request', hash_including({method: "GET", status: 200, host: "example.com"}))
+          Net::HTTP.get(URI.parse('http://example.com'))
+        end
       end
     end
   end
