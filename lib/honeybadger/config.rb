118034c8395dd79fada9979f9f519ded8e81234a
Roel Bondoc <roelbondoc@users.noreply.github.com>
2025-06-05 11:30:01 -0400

    feat: exclude insights from rake tasks config (#697)
    This introduces a new configuration option
    `insights.exclude_rake_tasks`. By default this includes the
    `assets:precompile` rake task commonly executed in Rails. This can be
    overridden to include other tasks when needed.

--- lib/honeybadger/config.rb
+++ lib/honeybadger/config.rb
@@ -289,7 +289,18 @@ module Honeybadger
     end
 
     def insights_enabled?
-      public? && !!self[:'insights.enabled']
+      return false unless public?
+      return false if insights_exclude_for_rake_tasks?
+      !!self[:'insights.enabled']
+    end
+
+    def insights_exclude_for_rake_tasks?
+      return false unless defined?(Rake) && Rake.application&.top_level_tasks
+
+      current_tasks = Array(Rake.application.top_level_tasks)
+      excluded_tasks = Array(self[:'insights.exclude_rake_tasks'])
+
+      (current_tasks & excluded_tasks).any?
     end
 
     def cluster_collection?(name)
