1b443ba81af3d49d37e868e456d0144c4a237a9b
Joshua Wood <josh@joshuawood.net>
2025-11-24 17:16:22 -0800

    Use the root NotificationSubscriber for now

--- lib/honeybadger/plugins/active_agent.rb
+++ lib/honeybadger/plugins/active_agent.rb
@@ -8,7 +8,8 @@ module Honeybadger
 
     execution do
       if config.load_plugin_insights?(:active_agent)
-        ::ActiveSupport::Notifications.subscribe(/(prompt|embed|stream_open|stream_close|stream_chunk|tool_call|process)\.active_agent/, Honeybadger::ActiveJobSubscriber.new)
+        # TODO: NotificationSubscriber may be the wrong thing to use here, or we may want to subclass it.
+        ::ActiveSupport::Notifications.subscribe(/(prompt|embed|stream_open|stream_close|stream_chunk|tool_call|process|provider)\.active_agent/, Honeybadger::NotificationSubscriber.new)
       end
     end
   end
