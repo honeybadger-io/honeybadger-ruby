12d501009033884dd77c0b4daedd12cfff1c5cae
Benjamin Curtis <ben@bencurtis.com>
2025-12-02 11:35:00 -0800

    feat: send Flipper events to Insights (#758)

---
+++ lib/honeybadger/plugins/flipper.rb
@@ -0,0 +1,33 @@
+require "honeybadger/notification_subscriber"
+
+module Honeybadger
+  module Plugins
+    module Flipper
+      Plugin.register :flipper do
+        requirement { defined?(::Flipper) }
+        requirement { defined?(::ActiveSupport::Notifications) }
+
+        execution do
+          if config.load_plugin_insights?(:flipper)
+            ::ActiveSupport::Notifications.subscribe(
+              "feature_operation.flipper",
+              Honeybadger::FlipperSubscriber.new
+            )
+          end
+        end
+      end
+    end
+  end
+end
+
+module Honeybadger
+  class FlipperSubscriber < NotificationSubscriber
+    def format_payload(payload)
+      payload.slice(:feature_name, :operation, :result)
+    end
+
+    def record(name, payload)
+      Honeybadger.event(name, payload)
+    end
+  end
+end
