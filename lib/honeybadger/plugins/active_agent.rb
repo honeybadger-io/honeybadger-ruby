7faa18626b3a5a2170fa239507b0a76e51cc9a82
Joshua Wood <josh@joshuawood.net>
2025-12-08 11:36:07 -0800

    Unsubscribe from the stream_chunk event
    This event results in too much logging.

--- lib/honeybadger/plugins/active_agent.rb
+++ lib/honeybadger/plugins/active_agent.rb
@@ -12,7 +12,7 @@ module Honeybadger
         execution do
           if config.load_plugin_insights?(:active_agent)
             ::ActiveSupport::Notifications.subscribe(
-              /(prompt|embed|stream_open|stream_close|stream_chunk|tool_call|process)\.active_agent/,
+              /(prompt|embed|stream_open|stream_close|tool_call|process)\.active_agent/,
               Honeybadger::ActiveAgentSubscriber.new
             )
           end
