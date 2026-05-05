d227e64947e554913c3b0a317be87b74f8217882
Benjamin Curtis <ben@bencurtis.com>
2026-05-05 06:38:32 -0700

    Ignore solid_cable_messages queries
    These are about as useful as  good_job and solid_queue queries, which we already ignore. :)

--- lib/honeybadger/config/defaults.rb
+++ lib/honeybadger/config/defaults.rb
@@ -33,7 +33,7 @@ module Honeybadger
 
     IGNORE_EVENTS_DEFAULT = [
       {event_type: "sql.active_record", query: /^(begin|commit)( immediate)?( transaction)?$/i},
-      {event_type: "sql.active_record", query: /(solid_queue|good_job)/i},
+      {event_type: "sql.active_record", query: /(solid_queue|good_job|solid_cable_messages)/i},
       {event_type: "sql.active_record", name: /^GoodJob/},
       {event_type: "process_action.action_controller", controller: "Rails::HealthController"},
       {event_type: "cache_read.active_support"},
